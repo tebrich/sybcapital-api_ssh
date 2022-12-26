dcc290c0bade03f84d89d5cc5dacdb29218cfaae
Arias-xss <kokiarias01@gmail.com>
2022-12-26 15:51:49 -0300

    Fix error on user/auth import

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UsersController],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   providers: [UsersService],
   exports: [UsersService]
 })
