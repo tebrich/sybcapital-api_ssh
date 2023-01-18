85d0f3065970b1c61cdbd7be6641ef2a8ba9c9d4
Arias-xss <kokiarias01@gmail.com>
2023-01-17 22:59:35 -0300

    Fix auth on header

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -6,7 +6,6 @@ import { CategoriesService } from './categories.service';
 import { CategoriesDto, UpdateCategoriesDto } from './dto/categories.dto';
 import { CategoriesFilterDto } from './dto/categories-filter.dto';
 
-@UseGuards(AuthGuard())
 @Controller('categories')
 @ApiTags('Categories')
 export class CategoriesController {
@@ -21,27 +20,26 @@ export class CategoriesController {
     async getOne(@Param('id', ParseIntPipe) id: number, @Query('includePosts') includePosts = false) {
         return await this.categoriesService.getOne(id, includePosts);
     }
-
+    @UseGuards(AuthGuard())
     @Post()
     async create(@Body() categoryDto: CategoriesDto) {
         return await this.categoriesService.create(categoryDto);
     }
-
+    @UseGuards(AuthGuard())
     @Patch('/:id')
     async update(@Param('id', ParseIntPipe) id: number, @Body() categoryDto: UpdateCategoriesDto) {
         return await this.categoriesService.update(id, categoryDto);
     }
-
+    @UseGuards(AuthGuard())
     @Delete('/:id')
     async delete(@Param('id', ParseIntPipe) id: number) {
         return await this.categoriesService.delete(id);
     }
-
+    @UseGuards(AuthGuard())
     @Get('/list')
     async getCategoriesList(@Body() categoriesFilter: string[]) {
         return await this.categoriesService.getCategoriesList(categoriesFilter);
     }
-
     @Get('/list/menu')
     async getCategoriesForMenu() {
         return await this.categoriesService.getCategoriesForMenu();
